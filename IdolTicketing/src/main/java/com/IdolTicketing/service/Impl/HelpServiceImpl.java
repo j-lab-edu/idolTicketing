f42ad984f6859b38838bf25971897f6d45441142
name <dhy9108@gmail.com>
2022-12-22 22:52:04 +0900

    ENH : helpController - create,update,delete,admin,get 생성 helpDTO 생성 helpMapper 생성 helpService 생성

---
+++ IdolTicketing/src/main/java/com/IdolTicketing/service/Impl/HelpServiceImpl.java
@@ -0,0 +1,38 @@
+package com.IdolTicketing.service.Impl;
+
+import com.IdolTicketing.dto.HelpDTO;
+import com.IdolTicketing.mapper.HelpMapper;
+import com.IdolTicketing.service.HelpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelpServiceImpl implements HelpService {
+    @Autowired
+    HelpMapper helpMapper;
+
+    @Override
+    public int helpBoard(HelpDTO helpDTO) {
+        return helpMapper.createBoard(helpDTO);
+    }
+
+    @Override
+    public int updateBoard(HelpDTO helpDTO) {
+        return helpMapper.updateBoard(helpDTO);
+    }
+
+    @Override
+    public int deleteBoard(HelpDTO helpDTO) {
+        return helpMapper.deleteBoard(helpDTO);
+    }
+
+    @Override
+    public HelpDTO getBoard(int id) {
+        return helpMapper.getBoard(id);
+    }
+
+    @Override
+    public int deleteBoardAdmin(HelpDTO helpDTO) {
+        return helpMapper.deleteBoardAdmin(helpDTO);
+    }
+}
